0bd31aa76c67447bef4d29451fcb4d716d7ec36c
Robson C Jesus <robson_c_j@hotmail.com>
2015-10-05 12:05:39 -0300

    Commit da Quinta mensagem.

--- Testes/src/Aluno.java
+++ Testes/src/Aluno.java
@@ -15,5 +15,6 @@ public class Aluno {
 		System.out.println("Testes Egit com github");
 		System.out.println("Terceira linha de mensagem");
 		System.out.println("Quarta mensagem");
+		System.out.println("Quinta mensagem");
 	}
 }
